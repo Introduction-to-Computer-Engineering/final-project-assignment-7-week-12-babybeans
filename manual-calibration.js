59931c221eaedef8f065fb177f0ad28c46d661d4
Jessica Gardner <48143153+babybeans@users.noreply.github.com>
2019-11-06 16:53:23 -0700

    Update manual-calibration.js

--- manual-calibration.js
+++ manual-calibration.js
@@ -1,100 +1,38 @@
-led.setBrightness(255)
-
-let reading = 0;
+let reading : number = 0;
 
 class Moisture {
+    constructor() { };//default constructor
+    
     getReading() {
-        pins.digitalWritePin(DigitalPin.P12, 1);
-        reading = pins.analogReadPin(AnalogPin.P0);
-        pins.digitalWritePin(DigitalPin.P12, 0);
+        pins.digitalWritePin(DigitalPin.P12, 1);//powers sensor
+        reading = pins.analogReadPin(AnalogPin.P0);//takes the reading
+        pins.digitalWritePin(DigitalPin.P12, 0);//turns off sensor
 
-        let i = pins.map(reading, 8, 815, 0, 4);
-
-        led.plot(0, 4 - i);
-        led.plot(1, 4 - i);
-        led.plot(2, 4 - i);
-        led.plot(3, 4 - i);
-        led.plot(4, 4 - i);
-        if (i > 0 && i < 1) {
-            this.ledRow5();
-            basic.pause(100)
-        }
-        if (i > 1 && i < 2) {
-            this.ledRow5();
-            this.ledRow4();
-            basic.pause(100)
-        }
-        if (i > 2 && i < 3) {
-            this.ledRow5();
-            this.ledRow4();
-            this.ledRow3();
-            basic.pause(100)
-        }
-        if (i > 3 && i < 4) {
-            this.ledRow5();
-            this.ledRow4();
-            this.ledRow3();
-            this.ledRow2();
-            basic.pause(100)
-        }
-        if (i > 4) {
-            this.ledRow1();
-            this.ledRow2();
-            this.ledRow3();
-            this.ledRow4();
-            this.ledRow5();
-            basic.pause(100)
-        }
+        let i : number = pins.map(reading, 8, 815, 0, 4);//maps reading on a scale of 0-4
+        this.ledRow(i);//displays row that corresponds with reading
+        this.fillRows(i);//displays all rows under row that corresponds with reading
 
     }
-    ledRow1() {
-        led.plot(0, 0);
-        led.plot(1, 0);
-        led.plot(2, 0);
-        led.plot(3, 0);
-        led.plot(4, 0);
+
+    fillRows(r  : number): void { //displays all rows of leds under i using ledRow function;
+        for (let i = r; i >= 0; i--) {
+            this.ledRow(i);
+        }
     }
-    ledRow2() {
-        led.plot(0, 1);
-        led.plot(1, 1);
-        led.plot(2, 1);
-        led.plot(3, 1);
-        led.plot(4, 1);
-    }
-    ledRow3() {
-        led.plot(0, 2);
-        led.plot(1, 2);
-        led.plot(2, 2);
-        led.plot(3, 2);
-        led.plot(4, 2);
-    }
-    ledRow4() {
-        led.plot(0, 3);
-        led.plot(1, 3);
-        led.plot(2, 3);
-        led.plot(3, 3);
-        led.plot(4, 3);
-    }
-    ledRow5() {
-        led.plot(0, 4);
-        led.plot(1, 4);
-        led.plot(2, 4);
-        led.plot(3, 4);
-        led.plot(4, 4);
-    }
-    ledRow(r: number): void {
-        led.plot(0, r);
-        led.plot(1, r);
-        led.plot(2, r);
-        led.plot(3, r);
-        led.plot(4, r);
+
+    ledRow(r: number): void {//displays a row of leds on the matrix
+        led.plot(0, 4 - r);
+        led.plot(1, 4 - r);
+        led.plot(2, 4 - r);
+        led.plot(3, 4 - r);
+        led.plot(4, 4 - r);
     }
 }
 
-basic.forever(function () {
+basic.forever(function () {//continually running
 
-    let test = new Moisture()
-    test.getReading();
+    let test = new Moisture();//instanciates Moisture class
+    test.getReading();//runs getreading function
     basic.pause(1000);
     basic.clearScreen();
     basic.pause(100);
